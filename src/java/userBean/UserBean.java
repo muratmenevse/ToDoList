c4c0bbcd5dc6c859d21b3d65fcc5bf8525c0ddae
hdemircioglu <h_demircioglu@windowslive.com>
2013-03-19 23:44:55 +0000

    Hasandan

--- src/java/userBean/UserBean.java
+++ src/java/userBean/UserBean.java
@@ -23,21 +23,40 @@ import userEJB.UserControllerLocal;
 @SessionScoped
 public class UserBean implements Serializable {
 
+    private String username;
+    private String password;
+    
 
 @EJB UserControllerLocal users;
     /** Creates a new instance of VisitorBean */
     public UserBean() {
     }
 
-   
+    public String getUsername() {
+        return username;
+    }
 
-    public List<User> getComments() {
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<User> getUsers() {
         return users.list();
     }
 
     public String submit() {
         User u = new User();
-      
+        u.setUsername(getUsername());
+        u.setPassword(getPassword());
+        u.setCreationTime(new Date());
         users.add(u);
         return "index.xhtml";
     }
