9c00e5bbd263d700fb869f09b34b21fe65358797
hdemircioglu <h_demircioglu@windowslive.com>
2013-03-19 23:27:04 +0000

    Hasandan

---
+++ src/java/taskBean/TaskBean.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package taskBean;
+
+
+import taskEJB.Task;
+import taskEJB.TaskControllerLocal;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author muratmenevse
+ */
+@Named(value="task")
+@SessionScoped
+public class TaskBean implements Serializable {
+
+
+@EJB TaskControllerLocal tasks;
+    /** Creates a new instance of VisitorBean */
+    public TaskBean() {
+    }
+
+   
+
+    public List<Task> getComments() {
+        return tasks.list();
+    }
+
+    public String submit() {
+        Task t = new Task();
+      
+        t.setDueDate(new Date());
+        tasks.add(t);
+        return "index.xhtml";
+    }
+
+    public String delete(Task c) {
+        tasks.delete(c);
+        return "index.xhtml";
+    }
+
+}
